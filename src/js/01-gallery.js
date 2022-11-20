8dd64bfcfa2fe8bcb28fee6ee01886a056a19593
Vladimir Larin <lvv0820@gmail.com>
2022-11-20 17:31:59 +0200

    start

---
+++ src/js/01-gallery.js
@@ -0,0 +1,8 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+// Описан в документации
+import SimpleLightbox from "/simplelightbox";
+// Дополнительный импорт стилей
+import "/simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
