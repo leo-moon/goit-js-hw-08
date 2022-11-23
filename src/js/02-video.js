09c5f68a5f27b619abffa9370c453cafaf6d1529
Vladimir Larin <lvv0820@gmail.com>
2022-11-23 02:16:35 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+player.on('timeupdate', throttle(playTrack, 1000));
+// player.on('timeupdate', function(e) {
+//   console.log('timeupdate',e)
+// })
+
+// function playTrack(www) {
+function playTrack({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
